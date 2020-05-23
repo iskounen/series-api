a58b481eca04fe56074b3ea1fdcd9dfa4c5f3786
Adam Iskounen <adam.iskounen@artsymail.com>
2020-05-23 07:47:27 -0400

    Adds the Book deletion flow.

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,7 +4,14 @@ class BooksController < ApplicationController
     @book = @series.books.create(book_params)
     redirect_to series_path(@series)
   end
- 
+
+  def destroy
+    @series = Series.find(params[:series_id])
+    @book = @series.books.find(params[:id])
+    @book.destroy
+    redirect_to series_path(@series)
+  end
+
   private
     def book_params
       params.require(:book).permit(:number)
