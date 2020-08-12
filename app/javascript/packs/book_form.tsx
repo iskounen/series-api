01758ed1294e5da724dfef9ea8b15c7d197bf060
Adam Iskounen <adam.iskounen@artsymail.com>
2020-08-12 18:26:20 -0400

    Installs TypeScript and updates the simple component.

--- app/javascript/packs/book_form.tsx
+++ app/javascript/packs/book_form.tsx
@@ -1,25 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import PropTypes from 'prop-types'
 
-const Hello = props => (
+interface HelloProps {
+  name: string
+}
+
+const Hello = (props: HelloProps) => (
   <div>Hello, {props.name}!</div>
 )
 
-Hello.defaultProps = {
-  name: 'world'
-}
-
-Hello.propTypes = {
-  name: PropTypes.string
-}
 
 document.addEventListener('DOMContentLoaded', () => {
   const node = document.getElementById('series_data')
   const data = JSON.parse(node.getAttribute('data'))
 
   ReactDOM.render(
-    <Hello />,
+    <Hello name="TypeScript" />,
     node,
   )
 })
